047cd6335ed956225f34347bbe56b73162d25d05
ferrojuand <ferrojuand@gmail.com>
2023-05-25 10:58:21 -0500

    Modificacion de style.css -responsive

---
+++ encriptador.js
@@ -0,0 +1,16 @@
+//Defincion de Funciones//
+function encriptar(){
+
+
+}
+
+function desencriptar(){
+
+}
+function copiar(){
+
+}
+
+//definicion de Variables//
+var texto_in="";
+var texto_out="Aun no hay ningun texto Encriptado";
