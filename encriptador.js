166af8371ceba5218bc1e3452a634afc37d8fd55
ferrojuand <ferrojuand@gmail.com>
2023-05-26 10:25:17 -0500

    Inclusion de funcionalidad - archivo encriptador.js

--- encriptador.js
+++ encriptador.js
@@ -1,16 +1,85 @@
 //Defincion de Funciones//
-function encriptar(){
+function encriptar(mensajeE) {
+    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    mensajeE = mensajeE.toLowerCase();
+
+    if (mensajeE =="") {
+        alert("Debes Ingresar el mensaje que quieres encriptar")
+        mensajeE="Ningún mensaje fue encontrado"        
+
+    } else {
+        for (let i = 0; i < codigo.length; i++) {
+            if (mensajeE.includes(codigo[i][0])) {
+                mensajeE = mensajeE.replaceAll(codigo[i][0], codigo[i][1])
+            }            
+        }     
+
+    }
+    return mensajeE;
+}
+function btnEncriptar() {
+    const mensaje = encriptar(textoIn.value);
+    textoOut.value = mensaje;
+    textoIn.value = "";
+
+}
+
+function desencriptar(mensajeE) {
+    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    mensajeE = mensajeE.toLowerCase();
+
+    if (mensajeE =="") {
+        alert("Debes Ingresar el mensaje que quieres encriptar")
+        mensajeE="Ningún mensaje fue encontrado"        
+
+    } else {
+        for (let i = 0; i < codigo.length; i++) {
+            if (mensajeE.includes(codigo[i][1])) {
+                mensajeE = mensajeE.replaceAll(codigo[i][1], codigo[i][0])
+            }            
+        }       
+
+    }
+    return mensajeE;  
+
+}
+
+function btnDesencriptar() {
+    const mensaje = desencriptar(textoIn.value);
+    textoOut.value = mensaje;
+    textoIn.value = "";
+
+}
+
+
+function copiar(data) {
+    navigator.clipboard.writeText(data);
+    textoIn.value = "";
+
+}
+function btnCopiar() {
+
+    copiar(textoOut.value); 
+     
+    
 
 
 }
 
-function desencriptar(){
 
-}
-function copiar(){
 
-}
+const textoIn = document.querySelector(".input_texto_in");
+const textoOut = document.querySelector(".input_texto_out");
 
-//definicion de Variables//
-var texto_in="";
-var texto_out="Aun no hay ningun texto Encriptado";
+
+
+
+
+
+
+
+
+
+
+
+
